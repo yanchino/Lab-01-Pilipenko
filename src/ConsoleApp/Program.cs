bd872ace5e0e802a12bf2dfac365825bcba3356e
Пилипенко Даниил <hery.god@mail.ru>
2026-03-28 11:57:47 +0700

    Initial commit: Lab 01 structure

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку для подсчета русских гласных:");
+            string input = Console.ReadLine();
+
+            
+            string vowels = "аеёиоуыэюя";
+            int count = 0;
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                
+                string lowerInput = input.ToLower();
+
+                foreach (char c in lowerInput)
+                {
+                    if (vowels.Contains(c))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Количество гласных букв: {count}");
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
